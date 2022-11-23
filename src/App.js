4550c26c0b3fb3346d8c2e4953eefb7454083aeb
ebenezergodwyll <ebenezergodwyll@gmail.com>
2022-11-23 15:29:26 +0000

    react-contacts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import {Component} from 'react';
+import {Container,Row,Col} from 'react-bootstrap';
+import Contacts from './Components/Contacts';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ContactsForm from './Components/ContactsForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props)
+  this.state={
+    contacts:[
+      { name: 'Mike',
+      phoneNumber: 1345764247,  
+      location: 'Oslo',
+    },
+
+      { name: 'Alex',
+      phoneNumber:  2345764247,   
+      location: 'Accra',
+    },
+
+      { name: 'Mia',
+      phoneNumber: 3345764247,
+      location: 'Berlin',
+    },
+      
+      { name: 'Sarah',
+      phoneNumber: 3342162137,
+      location: 'Kumasi',
+    },
+
+    
+  ]
+  }
+
+
+  }; 
+
+ handleAddContact=(newPerson)=> {
+
+  this.setState({
+    contacts:[...this.state.contacts,newPerson]
+  })
+
+  };
+
+
+
+  render(){
+   return <div className="App">
+    <Container>
+    <Row>
+    <Col><ContactsForm addContact={this.handleAddContact}/></Col>
+    <Col>
+     <Contacts people={this.state.contacts}/>
+    </Col>
+    </Row>
+    </Container> 
+          
+          </div>;
+
+  };
 }
 
-export default App;
+export default App
+
+
