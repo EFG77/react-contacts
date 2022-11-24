d015549f4349a06fc511358534f2377de9062772
ebenezergodwyll <ebenezergodwyll@gmail.com>
2022-11-24 14:14:44 +0000

    react-contacts final

--- src/Components/Contacts.jsx
+++ src/Components/Contacts.jsx
@@ -4,12 +4,13 @@ import Contact from'./Contact';
 const Contacts = (props) => {
     let staff=props.people
     return (
+
         <div>
-          {
-            staff.map( (item, index)=>{
+          {""}  
+          {staff.map((item,index)=>{
                 return(
-                    <div>
-                        <Contact person={item} key={index}/>
+                    <div key={index}>
+                        <Contact person={item}/>{""}
                     </div>
                 )
             })
